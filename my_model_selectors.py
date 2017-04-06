3b66a08f040e7b5b1a4560d483c6e7d93ee4280f
Ilan Stefanon <ilan.stefanon@gmail.com>
2017-04-07 00:41:16 +0200

    add select best model based on Bayesian information criteria

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -34,7 +34,7 @@ class ModelSelector(object):
     def base_model(self, num_states):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
@@ -49,7 +49,7 @@ class ModelSelector(object):
     def selected_model(self, num_states, x, lengths):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(x, lengths)
@@ -91,9 +91,31 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # model selection based on BIC scores
+
+        #number of states :
+        n = np.sum(self.lengths)
+
+        min_bic = 1000000
+        best_hmm_model = None
+        for nb_hidden_state in range(self.min_n_components, self.max_n_components + 1):
+            #model, logL = self.base_model(nb_hidden_state)
+            try:
+                hmm_model = GaussianHMM(n_components=nb_hidden_state, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+                logL = hmm_model.score(self.X, self.lengths)
+                logN = np.log(n)
+                # number of free states
+                p =  n^2 + 2* nb_hidden_state *n - 1
+                bic = -2 * logL + p * logN
+                if (bic < min_bic):
+                    min_bic = bic
+                    best_hmm_model = hmm_model
+            except:
+                continue
+        return best_hmm_model
 
 
 class SelectorDIC(ModelSelector):
@@ -120,12 +142,26 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
+        # model selection using CV
         n_splits = min(3, len(self.lengths))
         split_method = KFold(n_splits=n_splits)
-        sum_log = 0
-        for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
-            X_train, lengths_train = combine_sequences(cv_train_idx, self.sequences)
-            model, logL =  self.selected_model(3, X_train, lengths_train)
-            sum_log += logL
-        return model, sum_log / n_splits
+        max_mean_logL = -1000000
+        best_hidden_state = 0
+        for nb_hidden_state in range(self.min_n_components, self.max_n_components+1) :
+            #print('nb_hidden_state', nb_hidden_state)
+            sum_log = 0
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                X_train, lengths_train = combine_sequences(cv_train_idx, self.sequences)
+                try:
+                    model, logL =  self.selected_model(nb_hidden_state, X_train, lengths_train)
+                    sum_log += logL
+                except:
+                    break
+            mean_logL = sum_log / n_splits
+            #print('mean_logL', mean_logL)
+            if(mean_logL> max_mean_logL):
+                max_mean_logL = mean_logL
+                best_hidden_state = nb_hidden_state
+        #print('max_mean_logL', max_mean_logL)
+
+        return self.base_model(best_hidden_state)
